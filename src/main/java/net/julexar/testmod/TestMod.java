7277a31871f483012387dd3d7aed099fd87f7047
Julexar <viktor.thorin@gmail.com>
2023-05-22 10:04:12 +0200

    Removed unnecessary lines

--- src/main/java/net/julexar/testmod/TestMod.java
+++ src/main/java/net/julexar/testmod/TestMod.java
@@ -34,9 +34,7 @@ public class TestMod {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
