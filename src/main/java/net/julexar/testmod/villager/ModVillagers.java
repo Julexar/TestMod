9ddf497582cc0659313aae547abc14a93d3af8d2
Julexar <viktor.thorin@gmail.com>
2023-05-27 11:56:38 +0200

    Removed unused import statement

--- src/main/java/net/julexar/testmod/villager/ModVillagers.java
+++ src/main/java/net/julexar/testmod/villager/ModVillagers.java
@@ -11,7 +11,6 @@ import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.spongepowered.tools.obfuscation.ObfuscationData;
 
 import java.lang.reflect.InvocationTargetException;
 
